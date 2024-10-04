1556ac851e0b1c7dd84e8d885518aa36ee57e819
Henri Lunnikivi <heggggxa@gmail.com>
2024-10-04 21:19:17 +0300

    Sanitize target commentary

--- compiler/rustc_target/src/spec/targets/riscv32e_unknown_none_elf.rs
+++ compiler/rustc_target/src/spec/targets/riscv32e_unknown_none_elf.rs
@@ -3,7 +3,7 @@ use crate::spec::{Cc, LinkerFlavor, Lld, PanicStrategy, RelocModel, Target, Targ
 pub(crate) fn target() -> Target {
     Target {
         // The below `data_layout` is explicitly specified by the ilp32e ABI in LLVM. See also
-        // `self.options.llvm_abiname`.
+        // `options.llvm_abiname`.
         data_layout: "e-m:e-p:32:32-i64:64-n32-S32".into(),
         llvm_target: "riscv32".into(),
         metadata: crate::spec::TargetMetadata {
@@ -19,7 +19,7 @@ pub(crate) fn target() -> Target {
             linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
             linker: Some("rust-lld".into()),
             cpu: "generic-rv32".into(),
-            // The ILP32E ABI specifies the `data_layout`. See also `self.data_layout`.
+            // The ilp32e ABI specifies the `data_layout`
             llvm_abiname: "ilp32e".into(),
             max_atomic_width: Some(32),
             atomic_cas: false,
